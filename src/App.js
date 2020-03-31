3fd52e73fdfc1489743b476f0923c456a71930f1
Wallace Sidhrée <i@dreamyguy.com>
2020-03-31 23:07:03 +0200

    :eyes: Improve loading screen

--- src/App.js
+++ src/App.js
@@ -183,15 +183,20 @@ const App = () => {
       <div className="app align-center" style={{ paddingTop: headerTopPadding }}>
         {loading &&
           <div className="loading__text">
-            <h3>Fetching data and preparing it, please be patient.</h3>
-            <p>Number of cases is the default view, <em><b>but the data can be filtered</b> to show other calculations</em>.</p>
+            <h3>Fetching live data and preparing it.</h3>
+            <h4>Each number has a human story behind it!</h4>
             <p>
-              <em><b>Once the loading is done</b></em>, click on<br/>
+              <em><b>Once the loading is done</b></em>:<br/><br/>
               <span className="option option--settings centered">
                 <Icon icon="filter" size="24" />
               </span>
-              to activate filtering options.
+              Activate filtering options.<br/><br/>
+              <span className="option option--search centered">
+                <Icon icon="search" size="24" />
+              </span>
+              Search by country name.
             </p>
+            <p>Number of cases is the default view, <em><b>but the data can be filtered</b> to show other calculations</em>.</p>
           </div>
         }
         {!loading && stats && isNotEmptyArray(stats) &&
