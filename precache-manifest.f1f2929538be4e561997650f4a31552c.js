ec42a941877465510a042342050d3d6ee5712dde
Wallace Sidhrée <i@dreamyguy.com>
2020-04-02 23:20:18 +0200

    Updates

---
+++ precache-manifest.f1f2929538be4e561997650f4a31552c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12dbb1b3076f17a3d0c333b58e89deca",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "b7737a21b3720ca8bd7a",
+    "url": "/covid-19/static/css/main.093246f3.chunk.css"
+  },
+  {
+    "revision": "53cfe87a755dba549372",
+    "url": "/covid-19/static/js/2.718770ba.chunk.js"
+  },
+  {
+    "revision": "1bcefd02b7d05499a818042383aa8e83",
+    "url": "/covid-19/static/js/2.718770ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7737a21b3720ca8bd7a",
+    "url": "/covid-19/static/js/main.672eff80.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  }
+]);
